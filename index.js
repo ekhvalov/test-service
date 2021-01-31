c937aff3a6d49303f678c959ba7bed49df9a49e3
Vladimir <ekhvalov@gmail.com>
2021-01-31 12:32:46 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+let http = require('http');
+
+//Create a server
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('0.1.0');
+});
+server.listen(8080); //the server object listens on port 8080
+process.on('SIGINT', function () {
+    console.log('Exit.');
+    server.close();
+    process.exit();
+});
+console.log('Server started');
+// setTimeout(function () {
+//     server.close();
+//     console.log('Timeout');
+//     process.exit();
+// }, 3000)
